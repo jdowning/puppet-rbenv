b3654c486345b55e0d8094dab1efd26454b576cc
Justin Downing <justin@downing.us>
2013-01-18 10:38:28 -0500

    update manifest documentation

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -7,6 +7,7 @@ define rbenv::plugin {
   exec { "install-${name}":
     command => "git clone https://github.com/${plugin[0]}/${plugin[1]}",
     cwd     => "${rbenv::params::install_dir}/plugins",
+    path    => [ '/usr/bin' ],
     onlyif  => "test -d ${rbenv::params::install_dir}/plugins",
     unless  => "test -d ${rbenv::params::install_dir}/plugins/${plugin[1]}",
   }
