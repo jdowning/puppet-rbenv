f69aeb1401c7f936a50225dd9594cfbc54b8976b
Justin Downing <justin@apartmenttherapy.com>
2013-01-18 10:38:28 -0500

    update manifest documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,28 +4,15 @@
 #
 # === Parameters
 #
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
+# None
 #
 # === Variables
 #
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
+# None
 #
 # === Examples
 #
-#  class { rbenv:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
+#  class { rbenv: }
 #
 # === Authors
 #
