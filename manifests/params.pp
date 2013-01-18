80c0ec4f3c7c3a16820167b165534c993cf6ab10
Justin Downing <justin@downing.us>
2013-01-18 09:55:49 -0500

    update rbenv install, add ruby-build option

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,6 @@
 # == Class: rbenv::params
 #
 class rbenv::params {
-
-  $repo_path          = 'git://github.com/sstephenson/rbenv.git'
-  $repo_name          = 'rbenv'
-  $install_prefix     = '/usr/local'
-  $install_dir        = "${install_prefix}/${repo_name}"
-
+  $repo_path       = 'git://github.com/sstephenson/rbenv.git'
+  $install_dir     = '/usr/local/rbenv'
 }
