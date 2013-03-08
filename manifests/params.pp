07e97e491d46f5fe3563c6ff76d98953c21800f2
Justin Downing <justin@downing.us>
2013-03-08 17:05:35 -0500

    New version 0.0.4 * add owner, group parameters * remove rbenv install_dir recurse permissions (sloooooow) * move bundler install into ruby-build script * ensure permissions on install_dir, plugins, ruby-build

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,8 @@
 # == Class: rbenv::params
 #
 class rbenv::params {
-  $repo_path       = 'git://github.com/sstephenson/rbenv.git'
-  $install_dir     = '/usr/local/rbenv'
+  $repo_path   = 'git://github.com/sstephenson/rbenv.git'
+  $install_dir = '/usr/local/rbenv'
+  $owner       = 'root'
+  $group       = 'developers'
 }
