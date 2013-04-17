4002200b57acce3c7b8978277e99062bdacfa5f0
Justin Downing <justin@downing.us>
2013-04-17 15:53:03 -0400

    remove rbenv-bundler dependency for ruby::build

--- manifests/build.pp
+++ manifests/build.pp
@@ -8,7 +8,7 @@ define rbenv::build ($global=false) {
   }
 
   exec { "/tmp/ruby-build-${title}":
-    require => [ Package['build-essential'], Rbenv::Plugin['sstephenson/ruby-build'], Rbenv::Plugin['carsomyr/rbenv-bundler'] ],
+    require => [ Package['build-essential'], Rbenv::Plugin['sstephenson/ruby-build'] ],
     timeout => 1800,
     unless  => "/usr/bin/test -d /usr/local/rbenv/versions/${title}",
   }
