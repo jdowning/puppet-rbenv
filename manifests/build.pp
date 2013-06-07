83535f677c284f91a99af9fac09fc3ff010245d2
Justin Downing <justin@downing.us>
2013-06-07 14:41:01 -0400

    Update module to 0.1.0
    This is a major revision that removes the cludgy shell script formally used
    to manage ruby installs. Instead, the  define will use a series
    of execs to handle the installation and management of ruby installs.
    
    * Updated rbenv::gem to honor
    * Updated rbenv::plugin to honor
    * Add requirement for git class (puppetlabs/puppet-git)
    * Update tests to actually work
    * Add Vagrantfile for smoke testing

--- manifests/build.pp
+++ manifests/build.pp
@@ -1,15 +1,89 @@
-## DEFINE
-define rbenv::build ($global=false) {
-  include rbenv::params
+# == Define: rbenv::build
+#
+# === Variables
+#
+# [$install_dir]
+#   This is set when you declare the rbenv class. There is no
+#   need to overrite it when calling the rbenv::build define.
+#   Default: $rbenv::install_dir
+#   This variable is required.
+#
+# [$owner]
+#   This is set when you declare the rbenv class. There is no
+#   need to overrite it when calling the rbenv::build define.
+#   Default: $rbenv::owner
+#   This variable is required.
+#
+# [$group]
+#   This is set when you declare the rbenv class. There is no
+#   need to overrite it when calling the rbenv::build define.
+#   Default: $rbenv::group
+#   This variable is required.
+#
+# [$global]
+#   This is used to set the ruby to be the global interpreter.
+#   Default: false
+#   This variable is optional.
+#
+# === Examples
+#
+# rbenv::build { '2.0.0-p195': global => true }
+#
+# === Authors
+#
+# Justin Downing <justin@downing.us>
+#
+define rbenv::build (
+  $install_dir = $rbenv::install_dir,
+  $owner       = $rbenv::owner,
+  $group       = $rbenv::group,
+  $global      = false,
+) {
 
-  file { "/tmp/ruby-build-${title}":
-    content => template('rbenv/ruby-build.sh.erb'),
-    mode    => '0755',
-  }
-
-  exec { "/tmp/ruby-build-${title}":
-    require => [ Package['build-essential'], Rbenv::Plugin['sstephenson/ruby-build'] ],
+  Exec {
+    cwd     => $install_dir,
+    path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/', "${install_dir}/bin/" ],
     timeout => 1800,
-    unless  => "/usr/bin/test -d /usr/local/rbenv/versions/${title}",
   }
+
+  exec { "own-plugins-${title}":
+    command => "chown -R ${owner}:${group} ${install_dir}/plugins",
+    user    => 'root',
+    unless  => "/usr/bin/test -d ${install_dir}/versions/${title}",
+  }->
+  exec { "git-pull-rubybuild-${title}":
+    command => '/usr/bin/git reset --hard HEAD && /usr/bin/git pull',
+    cwd     => "${install_dir}/plugins/ruby-build",
+    user    => 'root',
+    unless  => "/usr/bin/test -d ${install_dir}/versions/${title}",
+  }->
+  exec { "rbenv-install-${title}":
+    command     => "${install_dir}/bin/rbenv install ${title}",
+    environment => ['CFLAGS=-O3 -march=native'],
+    creates     => "${install_dir}/versions/${title}",
+  }~>
+  exec { "bundler-install-${title}":
+    command     => "${install_dir}/shims/gem install bundler",
+    environment => ["RBENV_VERSION=${title}"],
+    refreshonly => true,
+  }~>
+  exec { "rbenv-rehash-${title}":
+    command     => 'rbenv rehash',
+    refreshonly => true,
+  }~>
+  exec { "rbenv-ownit-${title}":
+    command     => "chown -R ${owner}:${group} ${install_dir}/versions/${title} && chmod -R g+w ${install_dir}/versions/${title}",
+    user        => 'root',
+    refreshonly => true,
+  }
+
+  if $global == true {
+    exec { "rbenv-global${title}":
+      command     => "rbenv global ${title}",
+      require     => Exec["rbenv-install-${title}"],
+      subscribe   => Exec["rbenv-ownit-${title}"],
+      refreshonly => true,
+    }
+  }
+
 }
