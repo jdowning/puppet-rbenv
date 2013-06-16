bd1ef7b43ab04bc710b07219a559b25776c1a222
Stefan Dorosz <stefan@thaumazein.org>
2013-06-16 15:46:09 -0400

    Update dependencies for gem test

--- tests/gem.pp
+++ tests/gem.pp
@@ -1,2 +1,6 @@
+class { 'git': }
+package { 'build-essential': }
 class { 'rbenv': }
+rbenv::plugin { 'sstephenson/ruby-build': }
+rbenv::build { '1.9.3-p385': }
 rbenv::gem { 'bundler': ruby_version => '1.9.3-p385' }
