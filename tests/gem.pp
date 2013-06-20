8a8cc9c8da4397cf0faccb64a24437ba3bb60069
Stefan Dorosz <stefan@thaumazein.org>
2013-06-20 00:54:26 -0400

    Make dependencies in gem test more explicit

--- tests/gem.pp
+++ tests/gem.pp
@@ -1,6 +1,11 @@
 class { 'git': }
-package { 'build-essential': }
+package { 'build-essential': ensure => installed }
 class { 'rbenv': }
 rbenv::plugin { 'sstephenson/ruby-build': }
-rbenv::build { '1.9.3-p385': }
+
+rbenv::build { '1.9.3-p385':
+ global => true,
+ require => [Class['git'], Package['build-essential']],
+}
+
 rbenv::gem { 'bundler': ruby_version => '1.9.3-p385' }
