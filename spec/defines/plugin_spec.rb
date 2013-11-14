6007cfedae7a94fcbac35b8783a685d78dccb5ca
Justin Downing <justin@apartmenttherapy.com>
2013-11-14 12:37:49 -0500

    add permissions exec after gem install #17

--- spec/defines/plugin_spec.rb
+++ spec/defines/plugin_spec.rb
@@ -7,31 +7,11 @@ describe 'rbenv::plugin' do
     let(:params) do
       {
         :install_dir => '/usr/local/rbenv',
-        :owner => 'root',
-        :group => 'adm',
       }
     end
 
     it { should include_class('rbenv') }
-
     it { should contain_exec("install-sstephenson/ruby-build") }
-    #it { should contain_exec("install-ruby-build").with(
-    #  {
-    #    'command' => "/usr/bin/git clone git://github.com/sstephenson/ruby-build.git",
-    #    'cwd' => "/usr/local/rbenv/plugins",
-    #    'onlyif' => "/usr/bin/test -d /usr/local/rbenv/plugins",
-    #    'unless' => "/usr/bin/test -d /usr/local/rbenv/plugins/ruby-build",
-    #  }
-    #)}
-
     it { should contain_exec("rbenv-permissions-sstephenson/ruby-build") }
-    #it { should contain_exec('rbenv-permissions-ruby-build').with(
-    #  {
-    #    'command' => "chown -R root:adm /usr/local/rbenv && chmod -R g+w /usr/local/rbenv",
-    #    'refreshonly' => true,
-    #  }
-    #)}
-
   end
-
 end
