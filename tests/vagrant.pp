d1dcaa7308b52aac0e9bd326cf43a2949f052b26
Justin Downing <justin@downing.us>
2014-01-08 15:44:48 -0500

    update Vagrantfile, vagrant.pp to test install backup gem

--- tests/vagrant.pp
+++ tests/vagrant.pp
@@ -3,9 +3,11 @@ class { 'rbenv': group => $group }
 rbenv::plugin { 'sstephenson/ruby-build': }
 rbenv::plugin { 'sstephenson/rbenv-vars': }
 
+# these packages are required for nokogiri
+package { ['libxslt1-dev', 'libxml2-dev']: }->
 rbenv::build { '2.0.0-p247': global => true }
 
-rbenv::gem { 'thor':
-  version      => '0.18.1',
+rbenv::gem { 'backup':
+  version      => '3.9.0',
   ruby_version => '2.0.0-p247'
 }
