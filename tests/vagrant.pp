c33f0a1b534c4879f1e7e2af8f70fd411a555229
Hawk Newton <hawk.newton@gmail.com>
2014-01-24 17:32:48 -0800

    Fix deps for nokogiri in tests for redhat

--- tests/vagrant.pp
+++ tests/vagrant.pp
@@ -4,7 +4,12 @@ rbenv::plugin { 'sstephenson/ruby-build': }
 rbenv::plugin { 'sstephenson/rbenv-vars': }
 
 # these packages are required for nokogiri
-package { ['libxslt1-dev', 'libxml2-dev']: }->
+$nokogiri_deps = $::osfamily ? {
+  'RedHat' => ['libxslt-devel', 'libxml2-devel'],
+  default  => ['libxslt1-dev', 'libxml2-dev']
+}
+
+package { $nokogiri_deps: } ->
 rbenv::build { '2.0.0-p247': global => true }
 
 rbenv::gem { 'backup':
