d79b36fdc15aabd1781d0ae6fe2054493863849e
Justin Downing <justin@downing.us>
2014-07-21 15:53:22 -0400

    Add ability to auto-update rbenv and rbenv:plugin #29
    * `class { 'rbenv': latest => true }` will keep rbenv updated
    * `rbenv::plugin { 'sstephenson/ruby-build': latest => true}` will keep
      ruby-build definitions updated

--- spec/classes/rbenv_spec.rb
+++ spec/classes/rbenv_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe 'rbenv', :type => 'class' do
   let(:facts) { { :osfamily => 'Debian' } }
+  let(:params) do
+    {
+      :install_dir => '/usr/local/rbenv',
+      :latest      => true,
+    }
+  end
 
   it { should contain_exec('git-clone-rbenv').with(
     {
@@ -28,4 +34,6 @@ describe 'rbenv', :type => 'class' do
       'mode'    => '0775',
     }
   )}
+
+  it { should contain_exec("update-rbenv") }
 end
