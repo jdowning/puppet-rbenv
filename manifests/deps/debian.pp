067663fc18a05b86e342589c5acb329674b939ec
Paul Carey <paul@sourcelab.technology>
2015-06-10 11:40:44 +0100

    use ensure packages function

--- manifests/deps/debian.pp
+++ manifests/deps/debian.pp
@@ -3,39 +3,14 @@
 # This module manages rbenv dependencies for Debian $::osfamily.
 #
 class rbenv::deps::debian {
-  if ! defined(Package['build-essential']) {
-    package { 'build-essential': ensure => present }
-  }
-
-  if ! defined(Package['libreadline6-dev']) {
-    package { 'libreadline6-dev': ensure => present }
-  }
-
-  if ! defined(Package['libssl-dev']) {
-    package { 'libssl-dev': ensure => present }
-  }
-
-  if ! defined(Package['zlib1g-dev']) {
-    package { 'zlib1g-dev': ensure => present }
-  }
-
-  if ! defined(Package['libffi-dev']) {
-    package { 'libffi-dev': ensure => present }
-  }
-
-  if ! defined(Package['libyaml-dev']) {
-    package { 'libyaml-dev': ensure => present }
-  }
-
-  if ! defined(Package['libncurses5-dev']) {
-    package { 'libncurses5-dev': ensure => present }
-  }
-
-  if ! defined(Package['libgdbm3']) {
-    package { 'libgdbm3': ensure => present }
-  }
-
-  if ! defined(Package['libgdbm-dev']) {
-    package { 'libgdbm-dev': ensure => present }
-  }
+  ensure_packages([
+    'build-essential',
+    'libreadline6-dev',
+    'libssl-dev',
+    'zlib1g-dev',
+    'libffi-dev',
+    'libyaml-dev',
+    'libncurses5-dev',
+    'libgdbm3',
+    'libgdbm-dev' ])
 }
