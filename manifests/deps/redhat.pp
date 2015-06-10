9abcc8d7f74531541eeaac3cedafa73aed7210ef
Paul Carey <paul@sourcelab.technology>
2015-06-10 12:04:24 +0100

    updates redhat and suse dependancies to use ensure_packages

--- manifests/deps/redhat.pp
+++ manifests/deps/redhat.pp
@@ -3,47 +3,18 @@
 # This module manages rbenv dependencies for redhat $::osfamily.
 #
 class rbenv::deps::redhat {
-  if ! defined(Package['binutils']) {
-    package { 'binutils': ensure => present }
-  }
 
-  if ! defined(Package['gcc']) {
-    package { 'gcc': ensure => present }
-  }
-
-  if ! defined(Package['gcc-c++']) {
-    package { 'gcc-c++': ensure => present }
-  }
-
-  if ! defined(Package['make']) {
-    package { 'make': ensure => present }
-  }
-
-  if ! defined(Package['openssl-devel']) {
-    package { 'openssl-devel': ensure => present }
-  }
-
-  if ! defined(Package['readline-devel']) {
-    package { 'readline-devel': ensure => present }
-  }
-
-  if ! defined(Package['zlib-devel']) {
-    package { 'zlib-devel': ensure => present }
-  }
-
-  if ! defined(Package['libffi-devel']) {
-    package { 'libffi-devel': ensure => present }
-  }
-
-  if ! defined(Package['libyaml-devel']) {
-    package { 'libyaml-devel': ensure => present }
-  }
-
-  if ! defined(Package['ncurses-devel']) {
-    package { 'ncurses-devel': ensure => present }
-  }
-
-  if ! defined(Package['gdbm-devel']) {
-    package { 'gdbm-devel': ensure => present }
-  }
+  ensure_packages([
+    'binutils',
+    'gcc',
+    'gcc-c++',
+    'make',
+    'openssl-devel',
+    'readline-devel',
+    'zlib-devel',
+    'libffi-devel',
+    'libyaml-devel',
+    'ncurses-devel',
+    'gdbm-devel'
+    ])
 }
--- manifests/deps/suse.pp
+++ manifests/deps/suse.pp
@@ -3,43 +3,17 @@
 # This module manages rbenv dependencies for suse $::osfamily.
 #
 class rbenv::deps::suse {
-  if ! defined(Package['binutils']) {
-    package { 'binutils': ensure => present }
-  }
 
-  if ! defined(Package['gcc']) {
-    package { 'gcc': ensure => present }
-  }
-
-  if ! defined(Package['automake']) {
-    package { 'automake': ensure => present }
-  }
-
-  if ! defined(Package['openssl-devel']) {
-    package { 'openssl-devel': ensure => present }
-  }
-
-  if ! defined(Package['zlib-devel']) {
-    package { 'zlib-devel': ensure => present }
-  }
-
-  if ! defined(Package['libffi-devel']) {
-    package { 'libffi-dev': ensure => present }
-  }
-
-  if ! defined(Package['libyaml-devel']) {
-    package { 'libyaml-dev': ensure => present }
-  }
-
-  if ! defined(Package['ncurses-devel']) {
-    package { 'ncurses-dev': ensure => present }
-  }
-
-  if ! defined(Package['readline-devel']) {
-    package { 'readline-dev': ensure => present }
-  }
-
-  if ! defined(Package['gdbm-devel']) {
-    package { 'gdbm-dev': ensure => present }
-  }
+  ensure_packages([
+    'binutils',
+    'gcc',
+    'automake',
+    'openssl-devel',
+    'readline-devel',
+    'zlib-devel',
+    'libffi-devel',
+    'libyaml-devel',
+    'ncurses-devel',
+    'gdbm-devel'
+  ])
 }
