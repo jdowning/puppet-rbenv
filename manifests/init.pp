12f4930a74f01579999d5e7b0ccee00cbe45f54c
Sam Pearson <sam@sgp.me.uk>
2017-06-22 20:29:57 +0100

    Add manage_profile parameter (boolean)
     - Makes the distribution of a profile.d snippet optional
     - Defaults to true to maintain current behaviour

--- manifests/init.pp
+++ manifests/init.pp
@@ -45,6 +45,11 @@
 #   Default: true
 #   This variable is optional.
 #
+# [$manage_profile]
+#   Toggles the option to let the module install rbenv.sh into /etc/profile.d.
+#   Default: true
+#   This variable is optional.
+#
 # === Requires
 #
 # This module requires the following modules:
@@ -70,14 +75,15 @@
 # Copyright 2013 Justin Downing
 #
 class rbenv (
-  $repo_path   = 'https://github.com/rbenv/rbenv.git',
-  $install_dir = '/usr/local/rbenv',
-  $owner       = 'root',
-  $group       = $rbenv::params::group,
-  $latest      = false,
-  $version     = undef,
-  $env         = [],
-  $manage_deps = true,
+  $repo_path      = 'https://github.com/rbenv/rbenv.git',
+  $install_dir    = '/usr/local/rbenv',
+  $owner          = 'root',
+  $group          = $rbenv::params::group,
+  $latest         = false,
+  $version        = undef,
+  $env            = [],
+  $manage_deps    = true,
+  $manage_profile = true,
 ) inherits rbenv::params {
 
   validate_array($env)
@@ -106,10 +112,12 @@ class rbenv (
     mode   => '0775',
   }
 
-  file { '/etc/profile.d/rbenv.sh':
-    ensure  => file,
-    content => template('rbenv/rbenv.sh'),
-    mode    => '0775'
+  if $manage_profile {
+    file { '/etc/profile.d/rbenv.sh':
+      ensure  => file,
+      content => template('rbenv/rbenv.sh'),
+      mode    => '0775'
+    }
   }
 
   # run `git pull` on each run if we want to keep rbenv updated
