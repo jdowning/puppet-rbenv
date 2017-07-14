7b1be951c2fca44d209d4da43e64d3737d679f2c
James Mollison <jamesmollison@gmail.com>
2017-07-14 16:19:42 +1000

    Update plugin.pp
    Add $latestcron variable.  This gives the option of updating rbenv via cron, as opposed to puppet run.  Users have the option to to have noisy output (quiet by default) and to set the cron time using the cron resource within puppet.

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -9,9 +9,22 @@
 #   This variable is required.
 #
 # [$latest]
-#   This defines whether the plugin is kept up-to-date.
+#   This defines whether the plugin is kept up-to-date during a puppet run.
 #   Defaults: false
-#   This vaiable is optional.
+#   This variable is optional.
+#
+# [$latestcron]
+#   This defines whether the plugin is kept up-to-date via cron.
+#   Defaults: false
+#   This variable is optional
+# 
+# [$handleoutput]
+#  This is used with cron
+#  Default: >/dev/null 2>&1 (only output on error)
+#
+# [$day] [$hour] [$minute] [$weekday]
+#  Specify runtime of cron if configured as per https://docs.puppet.com/puppet/latest/types/cron.html
+#   Defaults: Sunday
 #
 # [$env]
 #   This is used to set environment variables when installing plugins.
@@ -31,9 +44,12 @@
 # Justin Downing <justin@downing.us>
 #
 define rbenv::plugin(
-  $install_dir = $rbenv::install_dir,
-  $latest      = false,
-  $env         = $rbenv::env,
+  $install_dir  = $rbenv::install_dir,
+  $latest       = false,
+  $latestcron   = false,
+  $handleoutput = '>/dev/null 2>&1',
+  $weekday      = 'Sunday',
+  $env          = $rbenv::env,
 ) {
   include rbenv
 
@@ -61,6 +77,17 @@ define rbenv::plugin(
       cwd     => "${install_dir}/plugins/${plugin[1]}",
       user    => $rbenv::owner,
       onlyif  => "/usr/bin/test -d ${install_dir}/plugins/${plugin[1]}",
+    }
+  }
+  # run `git pull` via cron to keep the plugin updated
+  if $latestcron == true {
+    cron { "rbenv-update-${name}":
+      command  => "cd ${install_dir}/plugins/${plugin[1]} ; /usr/bin/git pull ${handleoutput}",
+      user     => $rbenv::owner,
+      monthday => $day,
+      weekday  => $weekday,
+      hour     => $hour,
+      minute   => $minute,
     }
   }
 }
