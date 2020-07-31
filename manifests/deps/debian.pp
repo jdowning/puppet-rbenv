e3ee369a89149439b394dcd397dd5c34c0933d98
Florian Pfitzer <github@black-coding.com>
2020-07-31 05:10:30 +0200

    Fix dependencies for Debian buster (#89)

--- manifests/deps/debian.pp
+++ manifests/deps/debian.pp
@@ -4,6 +4,7 @@
 #
 class rbenv::deps::debian {
   $packages = $::lsbdistcodename ? {
+    'buster'           => [ 'libgdbm6', 'libssl-dev' ],
     'bionic'           => [ 'libgdbm5', 'libssl1.0-dev' ],
     default            => [ 'libgdbm3', 'libssl-dev']
   }
